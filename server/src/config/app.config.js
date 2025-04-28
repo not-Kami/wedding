979ae7a7bd5a958c2607d8852e6646abae2add4a
kami <kami-labs@proton.me>
2025-04-28 21:05:57 +0200

    fix: updating missing packages & addind the express.json & url encoded

--- server/src/config/app.config.js
+++ server/src/config/app.config.js
@@ -3,8 +3,13 @@ import database from './database.config.js';
 import userRouter from '../resources/user/user.route.js';
 import weddingRouter from '../resources/wedding/wedding.route.js';
 import loggerMiddleware from '../middleware/config.logger.js';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 database.then(() => {
     console.log('Connected to database');
 }).catch((err) => {
