394a59b5f114bddc8ec689d5e346c0c9330c41fc
kami <kami-labs@proton.me>
2025-04-30 18:52:51 +0200

    adding the missing vendors controller & correction on route imports

--- server/src/config/app.config.js
+++ server/src/config/app.config.js
@@ -3,8 +3,6 @@ import database from './database.config.js';
 import userRouter from '../resources/user/user.route.js';
 import weddingRouter from '../resources/wedding/wedding.route.js';
 import loggerMiddleware from '../middleware/config.logger.js';
-import cors from 'cors';
-import jwt from 'jsonwebtoken';
 
 const app = express();
 
@@ -16,8 +14,10 @@ database.then(() => {
     console.log('Error connecting to database', err);
 });
 
-app.use('/api/users', userRouter);
 app.use('/api/weddings', weddingRouter);
+app.use('/api/guests', guestRouter);
+app.use('/api/vendors', vendorRouter);
+app.use('/api/budgets', budgetRouter);
 app.use('/', loggerMiddleware);
 
 export default app;
---
+++ server/src/resources/vendor/vendor.controller.js
@@ -0,0 +1,24 @@
+import Vendor from './vendor.model';
+
+export const createVendor = async (req, res) => {
+    try{
+        const {name, type, contact} = req.body;
+        const vendor = new Vendor({name, type, contact});
+        await vendor.save();
+        res.status(201).json(vendor);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+export const getVendor = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const vendor = await Vendor.findById(id);
+        res.status(200).json(vendor);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+
